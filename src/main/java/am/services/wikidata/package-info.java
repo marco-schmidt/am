eb20c5311177570a7a1080b55ec59261d3745491
Marco Schmidt <mschmidtgit@protonmail.com>
2019-10-10 23:14:46 +0200

    add package-level API documentation

---
+++ src/main/java/am/services/wikidata/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Query content metadata at the Wikidata project. This can happen online via API endpoints or by parsing database
+ * dumps.
+ *
+ * @author Marco Schmidt
+ */
+package am.services.wikidata;

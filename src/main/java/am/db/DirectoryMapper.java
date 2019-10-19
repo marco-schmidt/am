331547a6a152b6b7ca74118d324b4900a0871c09
Marco Schmidt <mschmidtgit@protonmail.com>
2019-10-19 16:22:21 +0200

    bugfix: keep sqlite from removing leading zeroes
    sqlite accepts column type string but will then auto-convert strings representing numbers to integer values, removing leading zeroes; with column type text, values remain as they were provided

--- src/main/java/am/db/DirectoryMapper.java
+++ src/main/java/am/db/DirectoryMapper.java
@@ -97,8 +97,8 @@ public class DirectoryMapper extends ModelMapper<Directory>
   @Override
   String getTableDefinition()
   {
-    return TABLE_DIRS_VOLUME_REF + " bigint,\n" + TABLE_DIRS_PARENT_REF + " bigint,\n" + TABLE_DIRS_NAME + " string,\n"
-        + TABLE_DIRS_WIKIDATA_ENT_ID + " string\n";
+    return TABLE_DIRS_VOLUME_REF + " bigint,\n" + TABLE_DIRS_PARENT_REF + " bigint,\n" + TABLE_DIRS_NAME + " text,\n"
+        + TABLE_DIRS_WIKIDATA_ENT_ID + " text\n";
   }
 
   @Override

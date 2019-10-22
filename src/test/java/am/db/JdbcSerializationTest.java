582ff7fe51a85f76197fa967adf854641064d881
Marco Schmidt <mschmidtgit@protonmail.com>
2019-10-22 23:26:20 +0200

    updated jdbc serialization unit test to include saving

--- src/test/java/am/db/JdbcSerializationTest.java
+++ src/test/java/am/db/JdbcSerializationTest.java
@@ -15,11 +15,14 @@
  */
 package am.db;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import am.app.AppConfig;
 import am.filesystem.model.Directory;
+import am.filesystem.model.File;
 import am.filesystem.model.Volume;
 
 public class JdbcSerializationTest
@@ -71,14 +74,48 @@ public class JdbcSerializationTest
     io.setConfig(config);
     io.connect(null);
     io.createTables();
+
+    // insert volume
     final Volume vol = new Volume();
     final String path = "/db/path";
     vol.setPath(path);
+    final VolumeMapper volumeMapper = io.getVolumeMapper();
+    boolean inserted = volumeMapper.insert(io, vol);
+    Assert.assertTrue("Inserting volume works.", inserted);
+
+    // insert volume's root directory
+    final DirectoryMapper directoryMapper = io.getDirectoryMapper();
     final Directory root = new Directory();
     vol.setRoot(root);
-    final VolumeMapper volumeMapper = io.getVolumeMapper();
-    final boolean inserted = volumeMapper.insert(io, vol);
-    Assert.assertTrue("Inserting volume works.", inserted);
+    root.setVolumeRef(vol.getId());
+    inserted = directoryMapper.insert(io, root);
+    Assert.assertTrue("Inserting root directory works.", inserted);
+
+    // insert subdirectory
+    final Directory sub = new Directory();
+    sub.setName("sub");
+    sub.setParentRef(root.getId());
+    sub.setVolumeRef(vol.getId());
+    root.add(sub);
+    inserted = directoryMapper.insert(io, sub);
+    Assert.assertTrue("Inserting subdirectory works.", inserted);
+
+    // insert file
+    final File file = new File();
+    file.setName("file");
+    file.setDirectoryRef(sub.getId());
+    file.setVolumeRef(vol.getId());
+    file.setByteSize(Long.valueOf(0));
+    file.setLastModified(new Date());
+    sub.add(file);
+    final FileMapper fileMapper = io.getFileMapper();
+    inserted = fileMapper.insert(io, file);
+    Assert.assertTrue("Inserting file works.", inserted);
+
+    final List<Volume> vols = new ArrayList<Volume>();
+    vols.add(vol);
+    io.saveAll(vols);
+
     final List<Volume> volsOut = io.loadAll();
     Assert.assertEquals("Load all on one-element database returns list of size one.", 1, volsOut.size());
     final Volume loadedVolume = volsOut.get(0);
